3474aa310eccec55dda2ade2f6db87a01ff0d9a4
Subit Kumar Bhoi <bhoisubit@gmail.com>
2026-01-30 19:08:27 +0530

    tasks are now stored to local storage

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // This file initializes the application and connects all components
 
 console.log("Todo App Initialized");
+
+document.addEventListener("DOMContentLoaded", function () {
+  renderTasks();
+});
